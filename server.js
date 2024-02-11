dfc5f8e36a99ef1c1423a0424257bf023a527db9
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-02-11 17:34:50 +0530

    changes done for hosting

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from './routes/productRoutes.js';
+import path from 'path';
+
+
 /*
 register = http://localhost:8080/api/v1/auth/register
 
@@ -26,6 +29,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -33,12 +37,17 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 
-// test rest api
-app.get("/", (req, res) => {
-  res.send({
-    message: "hiiiiiiiiiiiiiiiiii",
-  });
+//web  hosting  route
+app.use("*",function(req,res){
+  res.sendFile(path.join(__dirname,'./client/build/index.html'));
 });
+
+// test rest api
+// app.get("/", (req, res) => {
+//   res.send({
+//     message: "hiiiiiiiiiiiiiiiiii",
+//   });
+// });
 
 // PORT
 const PORT = process.env.PORT || 8080;
